da911ae175d8d6639a5155d4d1fcf62e3f687de3
moumou1230 <gwangahn94@gmail.com>
2024-07-25 18:03:06 +0900

    완성

--- homework/mysql/board.js
+++ homework/mysql/board.js
@@ -1,6 +1,6 @@
 let sql = {
   boardList: "select * from board",
-  boardGet: "select * from board where ?",
+  boardGet: "select * from board where boardid =?",
   boardInsert: "insert into board set ?",
   boardUpdate: "update  board set  ? where boardid = ?",
   boardDelete: "delete from board where boardid =?",
--- homework/routes/board.js
+++ homework/routes/board.js
@@ -4,7 +4,27 @@ const router = express.Router();
 const mysql = require("../mysql/pool");
 
 router.get("/", (req, res) => {
-  mysql.query("boardList").then((resylt) => res.send(result));
+  mysql.query("boardList").then((result) => res.send(result));
 });
 
+router.get("/:boardid", (req, res) => {
+  let boardid = req.params.boardid;
+  mysql.query("boardGet", boardid).then((result) => res.send(result));
+});
+
+router.post("/", (req, res) => {
+  mysql.query("boardInsert", req.body).then((result) => res.send(result));
+});
+
+router.put("/:boardid", (req, res) => {
+  let boardid = req.params.boardid;
+  mysql
+    .query("boardUpdate", [req.body, boardid])
+    .then((result) => res.send(result));
+});
+
+router.delete("/:boardid", (req, res) => {
+  let boardid = req.params.boardid;
+  mysql.query("boardDelete", boardid).then((result) => res.send(result));
+});
 module.exports = router;
